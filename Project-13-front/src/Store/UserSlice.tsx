6fe7dc241747693ecdb99f79a986336db3b6a298
[ALexandre Toullec] <[alexandre.toullec@gmail.com]>
2024-01-03 15:22:53 +0100

    barnch fetch update

--- Project-13-front/src/Store/UserSlice.tsx
+++ Project-13-front/src/Store/UserSlice.tsx
@@ -1,55 +1,28 @@
-import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
-import axios from "axios";
+import { createSlice } from "@reduxjs/toolkit";
 
-export const loginUser = createAsyncThunk(
-  "user/loginUser",
-  async (userCredentials) => {
-    const request = await axios.post(
-      `http://localhost:3001/api/v1/user/login`,
-      userCredentials
-    );
-    const response = await request.data;
-    localStorage.setItem("user", JSON.stringify(response));
-    // console.log(response.data.body.token);
-
-    return response;
-  }
-);
-
-const loginSlice = createSlice({
+export const userSlice = createSlice({
+  // A string name for this slice of state. Generated action type constants will use this as a prefix.
   name: "user",
+  // The initial state value for this slice of state.
   initialState: {
-    loading: false,
-    user: null,
-    error: null,
+    token: "",
+    firstName: "",
+    lastName: "",
   },
-  extraReducers: (builder) => {
-    builder
-      .addCase(loginUser.pending, (state) => {
-        state.loading = true;
-        state.user = null;
-        state.error = null;
-      })
-      .addCase(loginUser.fulfilled, (state, action) => {
-        state.loading = false;
-        state.user = action.payload;
-        state.error = null;
-        console.log(action);
-      })
-      .addCase(loginUser.rejected, (state, action) => {
-        state.loading = false;
-        state.user = null;
-        console.log(action.error.message);
-        if (action.error.message === "Request failed with status code 400") {
-          state.error = "Access Denied! Invalid Credentials ";
-        } else {
-          state.error = action.message;
-        }
-
-        state.error = null;
-      });
+  // An object containing Redux "case reducer" functions (functions intended to handle a specific action type, equivalent to a single case statement in a switch).
+  reducers: {
+    itsToken: (state, action) => {
+      state.token = action.payload;
+    },
+    itsFirstname: (state, action) => {
+      state.firstName = action.payload;
+    },
+    itsLastname: (state, action) => {
+      state.lastName = action.payload;
+    },
   },
-  reducers: undefined,
 });
 
-export const useReducer = loginSlice.reducer;
+export const { itsToken, itsFirstname, itsLastname } = userSlice.actions;
+
+export const useReducer = userSlice.reducer;
--- Project-13-front/src/pages/login/Login.tsx
+++ Project-13-front/src/pages/login/Login.tsx
@@ -1,51 +1,68 @@
 import { useState } from "react";
 import "./login.scss";
 import { useDispatch, useSelector } from "react-redux";
-import { loginUser, userProfile } from "../../Store/UserSlice";
-import { useNavigate } from "react-router-dom";
+import { itsToken } from "../../Store/UserSlice";
+import App from "../home/Home";
+import { Link, useNavigate } from "react-router-dom";
 
 const SignIn = () => {
-  //states
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  // useState pour les inputs de connexion
+  const [itsInput, setItsInput] = useState({ email: "", password: "" });
+  // useState pour les erreurs (true/false)
+  const [error, setError] = useState(false);
 
-  //redux state
-  const { loading, error } = useSelector((state) => state.user);
+  // useSelector nous permet d'extraire les datas venant de l'état du store Redux
+  const token = useSelector((state) => state.user.token);
 
-  const dispatch = useDispatch();
-  const navigate = useNavigate();
-
-  const handleLoginEvent = (e) => {
-    e.preventDefault();
-    let userCredentials = {
-      email,
-      password,
-    };
-    dispatch(loginUser(userCredentials)).then((result) => {
-      if (result.payload) {
-        setEmail("");
-        setPassword("");
-        navigate("/user");
-        console.log(result.payload.body.token);
-      }
+  // Va permettre de récupérer la valeur des inputs et changer son état.
+  const inputChange = (e) => {
+    const inputID = e.target.id;
+    // Récupère tout ce qu'il y a dans 'itsInput' et ajoute la valeur du target dans {email:'', password:''} grâce à setItsInput.
+    setItsInput({
+      ...itsInput,
+      [inputID]: e.target.value,
     });
   };
+
+  // useDispatch ne peut pas être appelé dans un callback étant donné que c'est une fonction donc const.
+  const dispatchMethod = useDispatch();
+
+  const submitLogin = (e) => {
+    e.preventDefault();
+    fetch("http://localhost:3001/api/v1/user/login", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        email: itsInput.email,
+        password: itsInput.password,
+      }),
+    })
+      .then((response) => response.json())
+      .then((json) => {
+        // useDispatch est le seul moyen de déclencher un changement d'état. La fonction du reducer en question sera appelée de manière synchrone.
+        // Sa valeur de retour sera considérée comme le prochain état.
+        dispatchMethod(itsToken(json.body.token));
+      })
+      .catch((error) => {
+        setError(true);
+        setItsInput({
+          ...itsInput,
+        });
+        console.error("Problème = " + error);
+      });
+  };
+
+  if (token) return <Link to="/user" />;
 
   return (
     <main className="main bg-dark">
       <section className="sign-in-content">
         <i className="fa fa-user-circle sign-in-icon"></i>
         <h1>Sign In</h1>
-        <form onSubmit={handleLoginEvent}>
+        <form onSubmit={submitLogin}>
           <div className="input-wrapper">
             <label htmlFor="userEmail">userEmail</label>
-            <input
-              type="text"
-              required
-              id="userEmail"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
+            <input type="text" required id="userEmail" onChange={inputChange} />
           </div>
           <div className="input-wrapper">
             <label htmlFor="password">Password</label>
@@ -53,17 +70,14 @@ const SignIn = () => {
               type="password"
               required
               id="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
+              onChange={inputChange}
             />
           </div>
           <div className="input-remember">
             <input type="checkbox" id="remember-me" />
             <label htmlFor="remember-me">Remember me</label>
           </div>
-          <button className="sign-in-button">
-            {loading ? "Loading..." : "Login"}
-          </button>
+          <button className="sign-in-button">Login</button>
           {error && (
             <div className="alert alert-danger" role="alert">
               {error}
